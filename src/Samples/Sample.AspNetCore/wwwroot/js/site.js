989a3ac04b7c17d97050fd2e9e65b2dd26d0d48b
Daniel Sundqvist <daniel.sundqvist@authority.se>
2022-06-28 09:00:37 +0200

    Updated js for shippingHandler

--- src/Samples/Sample.AspNetCore/wwwroot/js/site.js
+++ src/Samples/Sample.AspNetCore/wwwroot/js/site.js
@@ -20,20 +20,19 @@ var updateSettings = function(element) {
 };
 
 
-var shippingHandler = new function (data) {
+var shippingHandler = function (data) {
     var a = data;
     console.log('event: ' + data);
 
     if (data) {
         document.dispatchEvent(new CustomEvent("sveaCheckout:setIsLoading", { detail: { isLoading: true } }));
-
+        console.log(data.type);
+        console.log(JSON.stringify(data.detail));
+        document.dispatchEvent(new CustomEvent("sveaCheckout:setIsLoading", { detail: { isLoading: false } }));
     }
 }
 
 
-
 $(function () {
-
-    document.addEventListener("sveaCheckout:shippingConfirmed", shippingHandler)
-
+    document.addEventListener("sveaCheckout:shippingConfirmed", shippingHandler);
 });

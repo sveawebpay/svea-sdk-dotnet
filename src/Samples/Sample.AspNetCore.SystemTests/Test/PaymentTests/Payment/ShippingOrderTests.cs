133f638f50e84ef29874ce043d0faad52733e2a2
Clement Joye <clement.joye@authority.se>
2022-07-05 09:46:12 +0200

    Updated shipping order test to use checkout api

--- src/Samples/Sample.AspNetCore.SystemTests/Test/PaymentTests/Payment/ShippingOrderTests.cs
+++ src/Samples/Sample.AspNetCore.SystemTests/Test/PaymentTests/Payment/ShippingOrderTests.cs
@@ -3,9 +3,11 @@ using NUnit.Framework;
 using Sample.AspNetCore.SystemTests.Services;
 using Sample.AspNetCore.SystemTests.Test.Base;
 using Sample.AspNetCore.SystemTests.Test.Helpers;
+using Svea.WebPay.SDK.CheckoutApi;
 using Svea.WebPay.SDK.PaymentAdminApi;
 using System;
 using System.Linq;
+using Checkout = Sample.AspNetCore.SystemTests.Test.Helpers.Checkout;
 
 namespace Sample.AspNetCore.SystemTests.Test.PaymentTests.Payment
 {
@@ -34,33 +36,21 @@ namespace Sample.AspNetCore.SystemTests.Test.PaymentTests.Payment
 
                     // Validate order info
                     .Orders.Last().Order.OrderStatus.Should.Equal(nameof(OrderStatus.Open))
-                    .Orders.Last().Order.PaymentType.Should.Equal(nameof(PaymentType.Card))
+                    .Orders.Last().Order.PaymentType.Should.Equal(nameof(Svea.WebPay.SDK.PaymentAdminApi.PaymentType.Card))
 
                     // Validate deliveries info
                     .Orders.Last().Deliveries.Should.HaveCount(0);
 
                 // Assert sdk/api response
-                var response = await _sveaClientSweden.PaymentAdmin.GetOrder(long.Parse(orderId)).ConfigureAwait(false);
+                var response = await _sveaClientSweden.Checkout.GetOrder(long.Parse(orderId)).ConfigureAwait(false);
 
                 Assert.That(response.Currency, Is.EqualTo("SEK"));
-                Assert.That(response.IsCompany, Is.False);
                 Assert.That(response.EmailAddress.ToString(), Is.EqualTo(TestDataService.Email));
-                Assert.That(response.OrderAmount.InLowestMonetaryUnit, Is.EqualTo(_amount * 100));
-                Assert.That(response.PaymentType.ToString(), Is.EqualTo(nameof(PaymentType.Card)));
-                Assert.That(response.OrderStatus.ToString(), Is.EqualTo(nameof(OrderStatus.Open)));
+                Assert.That(response.PaymentType.ToString(), Is.EqualTo(nameof(Svea.WebPay.SDK.CheckoutApi.PaymentType.SVEACARDPAY)));
+                Assert.That(response.Status.ToString(), Is.EqualTo(nameof(CheckoutOrderStatus.Final)));
 
-                CollectionAssert.AreEquivalent(
-                    new string[] { OrderActionType.CanDeliverOrder, OrderActionType.CanCancelOrder, OrderActionType.CanCancelAmount },
-                    response.AvailableActions
-                );
-                Assert.That(response.OrderRows.Count, Is.EqualTo(2));
-                Assert.That(response.OrderRows.First().AvailableActions, Is.Empty);
-                Assert.That(response.OrderRows.First().ArticleNumber, Is.EqualTo("bring_0.00"));
-                Assert.That(response.OrderRows.Last().AvailableActions, Is.Empty);
-                Assert.That(response.Deliveries, Is.Null);
-
-                //Assert.That(response.ShippingAddress.FirstName, Is.EqualTo(TestDataService.SwedishFirstName));
-                //Assert.That(response.ShippingAddress.LastName, Is.EqualTo(TestDataService.SwedishLastName));
+                Assert.That(response.ShippingAddress.FirstName, Is.EqualTo(TestDataService.SwedishFirstName));
+                Assert.That(response.ShippingAddress.LastName, Is.EqualTo(TestDataService.SwedishLastName));
                 Assert.That(response.ShippingAddress.StreetAddress, Is.EqualTo(TestDataService.ShippingStreetAddress));
                 Assert.That(response.ShippingAddress.PostalCode, Is.EqualTo(TestDataService.ShippingZipCode.Replace(" ", "")));
                 Assert.That(response.ShippingAddress.City, Is.EqualTo(TestDataService.ShippingCity.ToUpper()));

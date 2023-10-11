eb3754a9cded9732648fed32ac3b3447d4b44ab4
lounge <fredlunden@gmail.com>
2023-10-11 10:40:52 +0200

    fixes for ui tests

--- src/Samples/Sample.AspNetCore.SystemTests/PageObjectModels/Payment/Blocks/EntityBlock.cs
+++ src/Samples/Sample.AspNetCore.SystemTests/PageObjectModels/Payment/Blocks/EntityBlock.cs
@@ -5,10 +5,10 @@ namespace Sample.AspNetCore.SystemTests.PageObjectModels.Payment
     [ControlDefinition("div[@data-testid='country-identification-view']", ComponentTypeName = "Entity Block")]
     public class EntityBlock<TOwner> : Control<TOwner> where TOwner : PageObject<TOwner>
     {
-        [FindByClass("MuiTab-wrapper", Index = 0)]
+        [FindByClass("MuiTab-root", Index = 0)]
         public Text<TOwner> IsPrivate { get; set; }
 
-        [FindByClass("MuiTab-wrapper", Index = 1)]
+        [FindByClass("MuiTab-root", Index = 1)]
         public Text<TOwner> IsCompany { get; set; }
 
     }

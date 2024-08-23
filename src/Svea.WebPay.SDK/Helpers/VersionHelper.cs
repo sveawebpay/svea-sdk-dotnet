2d6fa7af2dd71542abb90ad1a3a8c0d4bcbd357e
Dušan Bobičić <dusan.bobicic@svea.com>
2024-08-23 15:41:18 +0200

    Add VersionHelper; Add SDK Version header

---
+++ src/Svea.WebPay.SDK/Helpers/VersionHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Svea.WebPay.SDK.Helpers
+{
+    internal static class VersionHelper
+    {
+
+        private static readonly string CachedVersion = GetVersionNumber();
+
+        public static string Version => CachedVersion;
+
+        private static string GetVersionNumber()
+        {
+            string fullVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString()??"1.0.0";
+            return fullVersion.Split('+')[0];
+        }
+
+    }
+}

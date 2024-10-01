49c9f766556dd1e20c2944470ba7e616a45938f0
Dušan Bobičić <dusan.bobicic@svea.com>
2024-10-01 12:12:51 +0200

    Fix naming of ChangePaymentMethodModel

--- src/Svea.WebPay.SDK/CheckoutApi/Recurring/CheckoutRecurring.cs
+++ src/Svea.WebPay.SDK/CheckoutApi/Recurring/CheckoutRecurring.cs
@@ -31,7 +31,7 @@ namespace Svea.WebPay.SDK.CheckoutApi.Recurring
             var data = await _sveaHttpClient.HttpGet<RecurringOrder>(url, configureAwait);
             return data;
         }
-        public async Task<ChangePaymentMethodResponse> ChangePaymentMethodAsync(ChangepaymentMethodModel changePaymentMethodModel, string recurringToken, bool configureAwait = false)
+        public async Task<ChangePaymentMethodResponse> ChangePaymentMethodAsync(ChangePaymentMethodModel changePaymentMethodModel, string recurringToken, bool configureAwait = false)
         {
             var url = new Uri($"/api/tokens/{recurringToken}/payment-methods", UriKind.Relative);
             var data = await _sveaHttpClient.HttpPost<ChangePaymentMethodResponse>(url, changePaymentMethodModel, configureAwait);
